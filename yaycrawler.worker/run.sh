92dafbefe2fdfea1558b972a416cb9f96f224e40
guoGuiBiao <1549310346@qq.com>
2017-08-09 16:00:26 +0800

    升级webmagico到0.7，升级spring-boot到了1.5.2.增加源码部署配置。修复包冲突问题。

--- yaycrawler.worker/run.sh
+++ yaycrawler.worker/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # export SPRING_PROFILES_ACTIVE=uat
 # nohup mvn -Puat -DskipTests spring-boot:run > ../smart-schedule.log 2>&1 &
-nohup mvn -Dspring.profiles.active=prod -DskipTests spring-boot:run >> ../yaycrawler-master.log 2>&1 &
+nohup mvn -Dspring.profiles.active=prod -DskipTests spring-boot:run >> ../logs/yaycrawler-worker.log 2>&1 &
